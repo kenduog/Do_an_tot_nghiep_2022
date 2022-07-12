84372cc052ccd52b3a8652d0fdb0f751f43ab4dc
kenduog <82100382+kenduog@users.noreply.github.com>
2022-07-12 09:33:23 +0700

    12_7_22_9_33

---
+++ WebQuanLyTruongHoc/wwwroot/Script/classroom.js
@@ -0,0 +1,65 @@
+﻿var app = angular.module('myApp', ['toastr']);
+app.controller('classroomCtrl', ['$scope', '$http', 'toastr', function ($scope, $http, toastr) {
+
+    $scope.grande = "";
+    $scope.key = "";
+    $scope.order = "";
+    $scope.teacherId = "";
+    $scope.year = "";
+
+    $scope.AddClassroom = function () {
+        if ($scope.grande != "" && $scope.key != "" && $scope.order != "" && $scope.teacherId != "" && $scope.year != "") {
+                var body = {
+                    "grande": $scope.grande,
+                    "key": $scope.key,
+                    "order": $scope.order,
+                    "teacherId": $scope.teacherId,
+                    "year": $scope.year                }
+            $http.post('https://localhost:44300/api/Classroom/CreateOrUpdateClassroom', JSON.stringify(body)).then(function (response) {
+                    if (response.data) {
+                        toastr.success('Đã thêm một dữ liệu mới', 'Thành công');
+                        $scope.grande = "";
+                        $scope.key = "";
+                        $scope.order = "";
+                        $scope.teacherId = "";
+                        $scope.year = "";
+                    }
+                    else
+                        toastr.error('Vui lòng thử lại.', 'Thêm thất bại!');
+                });
+            }
+        else {
+            toastr.warning('Vui lòng điền đủ thông tin.', 'Cảnh báo!');
+        }
+    }
+}]);
+
+var app = angular.module('myApp2', []);
+app.controller('classroomCtrl2', ['$scope', '$http', function ($scope, $http) {
+    $http({
+        method: "GET",
+        url: "https://localhost:44300/api/Classroom/GetClassroom"
+    }).then(function (response) {
+        $scope.listClassrooms = response.data.data;
+        $scope.totalClassroom = response.data.total;
+    });
+    //$scope.ID = "";
+    //$scope.GetIdDel = function (id) {
+    //    if (id != null) {
+    //        $scope.ID = id;
+    //    }
+    //}
+    //$scope.DelteteStudent = function () {
+    //    if ($scope.ID != null) {
+    //        $http.delete('https://localhost:44300/api/Students/DeleteStudentsEntity/' + $scope.ID).then(function (response) {
+    //            $http({
+    //                method: "GET",
+    //                url: "https://localhost:44300/api/Students/GetAllStudents"
+    //            }).then(function (response) {
+    //                $scope.listStudents = response.data.data;
+    //                $scope.totalStudent = response.data.total;
+    //            });
+    //        });
+    //    }
+    //};
+}]);
--- WebQuanLyTruongHoc/wwwroot/Script/student.js
+++ WebQuanLyTruongHoc/wwwroot/Script/student.js
@@ -11,47 +11,51 @@ app.controller('studentCtrl', ['$scope', '$http', 'toastr', function ($scope, $h
     $scope.password = "";
     $scope.comfirmPassword = "";
     $scope.status = "";
- 
-    $scope.EditStudent = function () {
-        
-        if ($scope.password == $scope.comfirmPassword) {
-            $http.post('https://localhost:44300/api/Students/CreateStudent', JSON.stringify(body)).then(function (response) {
-                if (response.data)
-                    toastr.success('Đã thêm một dữ liệu mới', 'Thành công');
-            });
-        }
-        else {
-            toastr.error('Vui lòng thử lại.', 'Mật khẩu không khớp!');
-        }
-    }
+
     $scope.AddStudent = function () {
-        
-        if ($scope.password == $scope.comfirmPassword) {
-            var body = {
-                "firstName": $scope.firstName,
-                "lastName": $scope.lastName,
-                "birthDate": $scope.birthDate,
-                "address": $scope.address,
-                "phone": $scope.phone,
-                "process": $scope.process,
-                "userName": $scope.userName,
-                "password": $scope.password
+        debugger
+        if ($scope.firstName != "" && $scope.lastName != "" && $scope.birthDate != "" && $scope.userName != "" && $scope.password != "") {
+            if ($scope.password == $scope.comfirmPassword) {
+                var body = {
+                    "firstName": $scope.firstName,
+                    "lastName": $scope.lastName,
+                    "birthDate": $scope.birthDate,
+                    "address": $scope.address,
+                    "phone": $scope.phone,
+                    "process": $scope.process,
+                    "userName": $scope.userName,
+                    "password": $scope.password
+                }
+                $http.post('https://localhost:44300/api/Students/CreateorUpdateStudent', JSON.stringify(body)).then(function (response) {
+                    if (response.data) {
+                        toastr.success('Đã thêm một dữ liệu mới', 'Thành công');
+                        $scope.firstName = "";
+                        $scope.lastName = "";
+                        $scope.birthDate = "";
+                        $scope.address = "";
+                        $scope.phone = "";
+                        $scope.process = "";
+                        $scope.userName = "";
+                        $scope.password = "";
+                        $scope.comfirmPassword = "";
+                        $scope.status = "";
+                    }
+                    else
+                        toastr.error('Vui lòng thử lại.', 'Thêm thất bại!');
+                });
             }
-            $http.post('https://localhost:44300/api/Students/CreateStudent', JSON.stringify(body)).then(function (response) {
-                if (response.data)
-                    toastr.success('Đã thêm một dữ liệu mới', 'Thành công');
-            });
+            else {
+                toastr.error('Vui lòng thử lại.', 'Mật khẩu không khớp!');
+            }
         }
         else {
-            toastr.error('Vui lòng thử lại.', 'Mật khẩu không khớp!');
+            toastr.warning('Vui lòng điền đủ thông tin.', 'Cảnh báo!');
         }
     }
 }]);
 
 var app = angular.module('myApp2', []);
 app.controller('studentCtrl2', ['$scope', '$http', function ($scope, $http) {
-    $scope.listStudents = [];
-    $scope.totalStudent = "";
     $http({
         method: "GET",
         url: "https://localhost:44300/api/Students/GetAllStudents"
@@ -59,4 +63,33 @@ app.controller('studentCtrl2', ['$scope', '$http', function ($scope, $http) {
         $scope.listStudents = response.data.data;
         $scope.totalStudent = response.data.total;
     });
+    $scope.ID = "";
+    $scope.GetIdStudent = function (id) {
+        if (id != null) {
+            $scope.ID = id;
+        }
+    }
+    $scope.DelteteStudent = function () {
+        if ($scope.ID != null) {
+            $http.delete('https://localhost:44300/api/Students/DeleteStudentsEntity/' + $scope.ID).then(function (response) {
+                $http({
+                    method: "GET",
+                    url: "https://localhost:44300/api/Students/GetAllStudents"
+                }).then(function (response) {
+                    $scope.listStudents = response.data.data;
+                    $scope.totalStudent = response.data.total;
+                });
+            });
+        }
+    };
+    $scope.listStudentDetail = [];
+    if ($scope.ID != null || $scope.ID != "") {
+        debugger
+        var bodyGetByStudentID = {
+            "StudentID": $scope.ID
+        }
+        $http.post('https://localhost:44300/api/Students/GetStudentByID', JSON.stringify(bodyGetByStudentID)).then(function (response) {
+            $scope.listStudentDetail = response.data.data;
+        });
+    };
 }]);
